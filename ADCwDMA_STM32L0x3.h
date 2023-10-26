8628623f2693b40701b7a7bb8dd9b611e4d0ccc5
Balazs Farkas <94067165+BFarkascode@users.noreply.github.com>
2023-10-26 13:58:09 +0200

    Add files via upload

---
+++ ADCwDMA_STM32L0x3.h
@@ -0,0 +1,24 @@
+/*
+ * ADCDMADriver_STM32L0x3.h		v.1.0.
+ *
+ *  Created on: Oct 26, 2023
+ *      Author: Balazs Farkas
+ */
+
+#ifndef INC_ADCWDMA_STM32L0X3_H_
+#define INC_ADCWDMA_STM32L0X3_H_
+
+#include "stdint.h"
+#include "stm32l053xx.h"								//device specific header file for registers
+
+//LOCAL CONSTANT
+
+//LOCAL VARIABLE
+
+//EXTERNAL VARIABLE
+
+//FUNCTION PROTOTYPES
+void ADCDMAInit(void);
+void ADCDMATempRead(DMA_Channel_TypeDef* dma_adc_channel_selector);
+
+#endif /* INC_ADCWDMA_STM32L0X3_H_ */
