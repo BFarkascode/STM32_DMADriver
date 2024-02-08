8d0657013e58d8ea17fda45d269197180f6d6f46
Balazs Farkas <94067165+BFarkascode@users.noreply.github.com>
2024-02-08 11:20:58 +0100

    Update DMADriver_STM32L0x3.c

--- DMADriver_STM32L0x3.c
+++ DMADriver_STM32L0x3.c
@@ -1,17 +1,11 @@
 /*
- *
  *  Created on: Oct 26, 2023
- *  Project: STM32_DMADriver
- *  File: DMADriver_STM32L0x3.c
  *  Author: BalazsFarkas
+ *  Project: STM32_DMADriver
  *  Processor: STM32L053R8
- *  Compiler: ARM-GCC (STM32 IDE)
  *  Program version: 1.0
- *  Program description: N/A
- *  Hardware description/pin distribution: N/A
- *  Modified from: N/A
- *  Change history: N/A
- *
+ *  File: DMADriver_STM32L0x3.c
+ *  Change history:
  *
  * v.1.0
  * Below is a custom DMA driver.
